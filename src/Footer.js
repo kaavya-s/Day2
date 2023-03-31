3853fea588a12096f109ca16290f3eddca0224b9
Kaavya <kaavya446@gmail.com>
2023-03-31 12:32:02 +0530

    first

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <h5>CONTACT US</h5>
+    <ul>
+        <li><p>Contact number:8924729300</p></li>
+        <li><p>Address:ABC,street-1,area,district.</p></li>
+        <li><p>sandbox@gmail.com</p></li>
+    </ul>
+    </div>
+  )
+}
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div >
+    <header>
+        <ul>
+            <li><img  className='img' src="https://img.freepik.com/premium-vector/sb-logo_590037-131.jpg?w=2000" ></img></li>
+            <li><h2>SANDBOX</h2>
+            <p>“cultural space with cafes, bookstore, galleries, and music stages”</p>
+            </li>
+        </ul>
+    </header>
+        <nav id="navi">
+            <ul>
+                <li><h6>Home</h6></li>
+                <li><h6>Featured Product</h6></li>
+                <li><h6>Contact Info</h6></li>
+            </ul>
+        </nav></div>
+  )
+}
